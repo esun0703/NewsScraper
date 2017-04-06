b81e30cf04504d4acc24fb6a3b0b39034daaac79
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-04-06 18:29:09 -0400

    btn handler

--- public/app.js
+++ public/app.js
@@ -2,14 +2,14 @@ $.getJSON("/articles", function(data){
 	for (var i = 0; i<data.length; i++){
 		//html version
 		// console.log(data)
-		$("#articleDisplay").append("<div class='articles'><h6 data-id='" + data[i]._id +"' class='title'>" + data[i].title + "</h6><a class='articleLink' href='" + data[i].link +"'>" + data[i].link + "</a><br/><p class='articleSummary'>" + data[i].summary + "</p><a class='btn-floating btn-small right'><i class='large material-icons'>mode_edit</i></a></div>");
+		$("#articleDisplay").append("<div class='articles'><h6 class='title'>" + data[i].title + "</h6><a class='articleLink' href='" + data[i].link +"'>" + data[i].link + "</a><br/><p class='articleSummary'>" + data[i].summary + "</p><a class='btn-floating btn-small right' data-id='" + data[i]._id +"'><i class='large material-icons'>mode_edit</i></a></div>");
 		
 
 		//handlebars version
 	}
 });
 
-$(document).on("click", "h6", function() {
+$(document).on("click", "a.btn-floating", function() {
 	$("#notes").empty();
 	var thisId = $(this).attr("data-id");
 	$.ajax({
